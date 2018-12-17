bd0b480c289f9b027f202e48b90789bdce2c1fb1
xiexuan <2621141526@qq.com>
2018-12-17 19:07:35 +0800

    添加javagit到本地仓库

---
+++ src/com/itxxx/domain/Hello.java
@@ -0,0 +1,18 @@
+package com.itxxx.domain;
+
+import java.io.Serializable;
+
+public class Hello implements Serializable {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("hello"+i);
+        }
+        System.out.println("helloWorld1");
+        System.out.println("helloWorld2");
+        System.out.println("helloWorld3");
+        System.out.println("helloWorld4");
+        System.out.println("helloWorld5");
+        System.out.println("helloWorld6");
+        System.out.println("helloWorld7");
+    }
+}
